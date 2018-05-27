5837c3cd3229557c6ed43a708d84baf6aafefbf1
invokerTc <592723124@qq.com>
2018-05-27 14:14:28 +0800

    branch b

---
+++ src/main/java/com/example/demo/dto/Gjd.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+/**
+ * Created by Administrator on 2018/5/27 0027.
+ */
+public class Gjd {
+    private Integer id;
+}
---
+++ src/main/java/com/example/demo/dto/Mouse.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+/**
+ * Created by Administrator on 2018/5/27 0027.
+ */
+public class Mouse {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}

d88ba953c5a81ba2c2d586ca292379455aa99c61
invokerTc <592723124@qq.com>
2018-05-27 13:43:21 +0800

    add dog

---
+++ src/main/java/com/example/demo/dto/Dog.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+/**
+ * Created by Administrator on 2018/5/27 0027.
+ */
+public class Dog {
+    private String style;
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
